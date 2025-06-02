d628433141c138d0e895ebe8152038785d8ced26
Artur Pagaev <paagaev@mail.ru>
2025-06-02 18:58:21 +0700

    Обновил pom.xml и тесты для Cat

--- src/test/java/animals/CatTest.java
+++ src/test/java/animals/CatTest.java
@@ -10,11 +10,20 @@ public class CatTest {
 
     @ParameterizedTest
     @ValueSource(ints = {0, 1, 2, 5})
-    void getKittensReturnsExpectedCount(int kittensCount) {
-        Feline feline = mock(Feline.class);
-        when(feline.getKittens()).thenReturn(kittensCount);
-        Cat cat = new Cat(feline);
+    void getKittensReturnsExpectedCount(int ignoredCount) {
+        // Создаем реальный объект Feline
+        Feline realFeline = new Feline();
 
-        assertEquals(kittensCount, cat.getKittens());
+        // Оборачиваем его в spy
+        Feline spyFeline = spy(realFeline);
+
+        // Переопределяем метод getKittens() чтобы всегда возвращал 1
+        when(spyFeline.getKittens()).thenReturn(1);
+
+        Cat cat = new Cat(spyFeline);
+
+        // Проверяем, что cat.getKittens() всегда 1
+        assertEquals(1, cat.getKittens());
     }
 }
+
